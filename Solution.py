deac1bcf4dbf259a94181758be9e73976f8d3996
Kasania <nasang1997@gmail.com>
2018-08-26 23:19:55 +0900

    Solution #11721

--- Solution.py
+++ Solution.py
@@ -1,4 +1,11 @@
-#Solution Number : #
+#Solution Number : #11721
 #-----------------------
 
-
+quotation = input()
+n = 0
+length = len(quotation)
+rep = length // 10
+while n < rep:
+    print(quotation[n*10:(n+1)*10])
+    n += 1
+print(quotation[n*10:])
