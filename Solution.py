1a7840f81a40389510629e3ef2a862b64d198be3
Kasania <nasang1997@gmail.com>
2018-08-26 23:51:43 +0900

    Solution #10817

--- Solution.py
+++ Solution.py
@@ -1,14 +1,4 @@
-#Problem Number : #9498
+#Problem Number : #10817
 #-----------------------
 
-score = int(input())
-if score >= 90 :
-    print('A')
-elif score >= 80 :
-    print('B')
-elif score >= 70 :
-    print('C')
-elif score >= 60 :
-    print('D')
-else:
-    print('F')
+print(sorted(map(int, input().split()))[1])
