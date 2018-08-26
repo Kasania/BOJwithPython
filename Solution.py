544e1c7ace0e46a048e931a004dd78e4de4b94cf
Kasania <nasang1997@gmail.com>
2018-08-27 00:08:20 +0900

    Solution #10871

--- Solution.py
+++ Solution.py
@@ -1,4 +1,14 @@
-#Problem Number : #10817
+#Problem Number : #10871
 #-----------------------
 
-print(sorted(map(int, input().split()))[1])
+condition = list(map(int,input().split()))
+numbers = list(map(int,input().split()))
+
+i = 0
+nList = list()
+length = len(numbers)
+while i < length:
+    if numbers[i] < condition[1]:
+        nList.append(numbers[i])
+    i += 1
+print(' '.join(map(str,nList)))
