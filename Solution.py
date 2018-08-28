c6b3dc890c01811206f963f46e2ffdb8abc93d3c
Kasania <nasang1997@gmail.com>
2018-08-28 22:37:39 +0900

    Solution #1546

--- Solution.py
+++ Solution.py
@@ -1,14 +1,7 @@
-#Problem Number : #10871
+#Problem Number : #1546
 #-----------------------
 
-condition = list(map(int,input().split()))
-numbers = list(map(int,input().split()))
-
-i = 0
-nList = list()
-length = len(numbers)
-while i < length:
-    if numbers[i] < condition[1]:
-        nList.append(numbers[i])
-    i += 1
-print(' '.join(map(str,nList)))
+numOfSubject = int(input())
+scoresOfSubject = sorted(map(int,input().split()))
+avg = (sum(scoresOfSubject) / numOfSubject) / scoresOfSubject[-1] * 100
+print(avg)
