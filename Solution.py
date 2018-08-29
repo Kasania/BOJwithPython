66310655aa4ad20a21eb3b0245e17156140e6729
Kasania <nasang1997@gmail.com>
2018-08-29 23:19:26 +0900

    Solution #1152

--- Solution.py
+++ Solution.py
@@ -1,17 +1,6 @@
-#Problem Number : #1463
+#Problem Number : #1152
 #-----------------------
 
-X = int(input())
+print(len(input().split()))
 
-resultArray = [9999]*(X+2)
-resultArray[1] = 0
-for i in range(1,X+1):
-    if i*3 <= X:
-        if resultArray[i * 3] > resultArray[i]+1:
-            resultArray[i * 3] = resultArray[i]+1
-    if i*2 <= X:
-        if resultArray[i * 2] > resultArray[i]+1:
-            resultArray[i * 2] = resultArray[i]+1
-    if resultArray[i + 1] > resultArray[i]+1:
-        resultArray[i + 1] = resultArray[i]+1
-print(resultArray[X])
+
