dc65dc9ac5b63d666e263788e3968fcfa50c2ace
Kasania <nasang1997@gmail.com>
2018-09-03 23:49:53 +0900

    Solution #1011

--- Solution.py
+++ Solution.py
@@ -1,20 +1,19 @@
-#Problem Number : #1193
+#Problem Number : #1011
 #-----------------------
 
-inputNumber = int(input())
-sumNum = 1
-tNum = 0
+import math
 
-while inputNumber > tNum:
-    tNum += sumNum
-    sumNum += 1
+testCase = int(input())
 
-M = 0
-S = 0
-if sumNum % 2 == 0:
-    S = tNum - inputNumber + 1
-    M = sumNum - S
-else:
-    M = tNum - inputNumber + 1
-    S = sumNum - M
-print('%d/%d'%(S,M))
+for T in range(testCase):
+    pos = list(map(int,input().split()))
+    gap = pos[1] - pos[0]
+    i = 1
+    # 1+2+3+...+ n +...+3+2+1 = n^2
+    while i*i <= gap:
+        i += 1
+    i -= 1
+
+    # n^2에 속하지 못한 부분의 길이
+    gap = math.ceil( (gap-i*i) / i)
+    print(i*2 - 1 + gap)
