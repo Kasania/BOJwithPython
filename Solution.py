8d4cfc2113362f71cfdbd88464f05e5b73d26b3e
Kasania <nasang1997@gmail.com>
2018-09-06 23:14:17 +0900

    Solution #10250 : Using formula

--- Solution.py
+++ Solution.py
@@ -5,16 +5,8 @@ testCase = int(input())
 
 for T in range(testCase):
     H,W,N = map(int,input().split())
-
-    Y = 0
-    X = 1
-    cnt = 0
-    while True:
-        if Y > H :
-            Y = 1
-            X += 1
-        if cnt == N:
-            break
-        Y += 1
-        cnt += 1
+    Y = N % H
+    if Y == 0:
+        Y = H
+    X = (N - 1) // H + 1
     print( Y*100 + X)
