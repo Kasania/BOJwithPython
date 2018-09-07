b43781d8c22214219ea38d61613e4bda68d355e7
Kasania <nasang1997@gmail.com>
2018-09-08 00:11:43 +0900

    Solution #2775

--- Solution.py
+++ Solution.py
@@ -1,12 +1,21 @@
-#Problem Number : #10250
+#Problem Number : #2775
 #-----------------------
 
-testCase = int(input())
+apt = [[0 for x in range(15)] for y in range(15)]
+for i in range(1,15):
+    apt[0][i] = i
 
-for T in range(testCase):
-    H,W,N = map(int,input().split())
-    Y = N % H
-    if Y == 0:
-        Y = H
-    X = (N - 1) // H + 1
-    print( Y*100 + X)
+for y in range(1,15):
+    for x in range(1,15):
+        if x == 1:
+            apt[y][x] = apt[y-1][x]
+        else:
+            cnt = 0
+            for k in range (1,x+1):
+                cnt += apt[y-1][k]
+            apt[y][x] = cnt
+
+for T in range(int(input())):
+    k = int(input())
+    n = int(input())
+    print(apt[k][n])
